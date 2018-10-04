f3a7d8b009c77f074d05ee9b2ca4238c6c178b0e
Serhan Ramadan <serhan_ramadan@yahoo.co.uk>
2018-10-04 17:51:10 +0100

    Updates

---
+++ content/Purrfect Match/script.js
@@ -0,0 +1,10 @@
+//var firstName = prompt("What is your first name?");
+//var lastName = prompt("What is your last name?");
+//var age = prompt("What is your age?");
+
+//console.log("Your full name is " + firstName + " " + lastName);
+//console.log("you are " + age + " years old ");
+
+//var age = prompt("What is your age");
+//var days = age * 365;
+//alert(age + " years is roughly " + days + " days");
